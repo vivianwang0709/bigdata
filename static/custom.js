6742dec267c9d8f2497f5b14d4d94dd33ce530f7
vivian <lincc7979@gmail.com>
2017-04-08 16:48:58 +0800

    add v2
    v2 finally version

--- static/custom.js
+++ static/custom.js
@@ -1,69 +1,87 @@
-$.getdata = function(path,count){
-    $.ajax({
-        url: path, //json文件位置
-        type: "GET", //请求方式为get
-        dataType: "json", //返回数据格式为json
-        success: function(data) {
++function ($) {
+	'use strict';
+	
+	var Article = function () {
+		this.count = 1
+		this.path  = null
+  	}
+
+  	Article.prototype.getPath = function (url) {
+
+		if (url.length==1){
+    		this.path = '../static/json/post.json';
+
+		}else{
+    		var reg = new RegExp("/article/",'g');
+    		var url = url.replace(reg,'');
+    		this.path = '../static/json/'+url+'.json';
+		}
+    	return this.path
+  	}
+
+  	Article.prototype.getData = function (path,count) {
+
+  		$.ajax({
+        	url: path, //json文件位置
+        	type: "GET", //请求方式为get
+        	dataType: "json", //返回数据格式为json
+        	success: function(data) {
+
             //请求成功完成后要执行的方法 
             //each循环 使用$.each方法遍历返回的数据date
             $.each(data, function(i, item) {
-                //if(count<0 || (i<=3*count-1 && i>=3*count-3)){
-                if(item.pageid<=3*count && item.pageid>=3*count-2){
-                var h = $(document).height();
-                $(".container").css("height",h+190); 
-                $(".m_left").append(function(n) {
-                    return "<div class='media'>\
-              <div class='media-left'>\
-              <a href='../article/"+ item.tag +"?pageid=" +item.pageid+ "'>\
-               <img class='media-object' src='../static/pic/"+item.tag+"_"+item.pageid+"_1.jpg' alt='...'>\
-              </a>\
-                    </div>\
-                    <div class='media-body'>\
-                        <h4 class='media-heading'>" + item.title + "</h4>\
-                        <p>" + item.content + "</p>\
-                        <p><a class='btn btn-primary btn-lg' href='../article/"+ item.tag +"?pageid=" +item.pageid+ "' role='button'>Read more</a></p>\
-                    </div>\
-                </div>"
-
-                });
-            }
-
+                
+                if(i<=3*count && i>=3*count-2){
+                	$(".post").append(function(n){
+                    return "<div class='article'><div class='a_img'><a href='../article/"+ item.tag +"?pageid=" +item.pageid+ "'><img src='../static/pic/"+item.tag+"_"+item.pageid+"_1.jpg' alt=''></a></div><div class='a_right'><div class='a_title'><h3>" + item.title + "</h3></div><div class='a_editor'></div><div class='a_date'></div><div class='a_content'><p>"+ item.content + "</p></div><div class='a_tag'><p><a class='btn btn-primary pull-right' href='../article/"+ item.tag +"?pageid=" +item.pageid+ "'>Read more<span class=''></span></a></p></div></div></div>"
+                	});
+            	}
             })
-        }
+
+        	}	
+    	})
+
+  		this.count = count + 1
+  		return this.count
+  	}
+
+
+ 	function Plugin(target) {
+
+  		var $this   = $(this)
+    	var url 	= window.location.pathname;
+    	var data    = $this.data('bs.post')
+    	
+		if (!data) {
+			$this.data('bs.post', (data = new Article()))
+			data.getPath(url)
+		}
+		
+		data.getData(data.path,data.count);
+
+		return this
+
+    }
+
+	$(window).scroll(function(){
+		
+		//滚动条所在位置的高度
+		var totalheight = parseFloat($(window).height()) + parseFloat($(window).scrollTop());
+
+		//当前文档高度   小于或等于   滚动条所在位置高度  则是页面底部
+		if(($(document).height()) <= totalheight+10) {
+  		var $target = $(".post")
+			Plugin.call($target,$target.data())
+		}
     })
-}
+    
 
+	$(document).ready(function() {
+		$('[data-ride="post"]').each(function () {
+			var $this = $(this);
+      		Plugin.call($this,$this.data())
+    	})
+  	})
+  	
 
-var url = window.location.pathname;
-
-if (url.length==1){
-    var path = '../static/json/post.json';
-
-}else{
-    var reg = new RegExp("/article/",'g');
-    var url = url.replace(reg,'');
-    var path = '../static/json/'+url+'.json';
-}
-
-
-var count=1
-
-$(document).ready(function() {
-    $.getdata(path,count);
-    count = count+1;
-});
-
-
-
-$(window).scroll(function(){
-//滚动条所在位置的高度
-totalheight = parseFloat($(window).height()) + parseFloat($(window).scrollTop());
-//当前文档高度   小于或等于   滚动条所在位置高度  则是页面底部
-if(($(document).height()) <= totalheight+10) {
-    $.getdata(path,count);
-    count = count+1;
- 
-}
-});
-
-
+}(jQuery);
