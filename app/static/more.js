75e5e7ed39de9314b8eedc67ab6fe690df15fdfa
vivian <lincc7979@gmail.com>
2017-07-03 12:59:06 +0800

    add v3.5.6

--- app/static/more.js
+++ app/static/more.js
@@ -10,7 +10,7 @@
 
       var max = $('.post').data('num')
       
-      if (num-1>max/5){
+      if (num-1>=max/5){
           $btn.css("display","none")
           $('.morediv').append('<p>親，感謝你的支持，已經加載到底部摟～</p>')
           return
